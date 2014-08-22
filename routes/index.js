9db5f7d4db8f602515f2b4e5918a6eaa3660d588
Robert Schmid <robert@theschmids.org>
2014-08-21 21:41:42 -0700

    Excluded a bunch of stuff

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+    // get standard header values for testing purpose
     var host = req.header('Host') || 'unknown';
     var useragent = req.header('User-Agent') || 'unknown';
-
+    // get custom MLB header values
     var userid = req.header('X-MLBAM-WUID') || 'unknown';
+
     res.render('index', { title: 'Simple WUID Test App' , userid: userid, host: host, useragent: useragent});
 });
 
